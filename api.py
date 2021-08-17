05336e5829e15488daa2cdfa59a8fb34381103ab
Paweł Sadowski <pawel.sadowski@ithaca.pl>
2021-08-17 13:44:57 +0200

    2to3 #2

--- api.py
+++ api.py
@@ -9,16 +9,16 @@ from sqlalchemy.exc import (IntegrityError)
 from sqlalchemy.orm.exc import (NoResultFound)
 from sqlalchemy.sql import func
 from sqlalchemy import case
+from sqlalchemy.orm import aliased
 import datetime
 import dateutil
 
 
 #
-def object_to_dict(Object):
-    dictionary = Object.__dict__.copy()
-    #del(dictionary['_sa_instance_state'])
-    return dictionary
-
+def object_to_dict(obj):
+    return {
+        f: getattr(obj, f) for f in type(obj)._fields
+    }
 
 def user_add(session, login, password, email):
     user = User(
@@ -105,7 +105,7 @@ def account_list_groupped(session, user_id):
         all()
     accounts = {}
     for account in account_list:
-        if account.account_type_group not in accounts.keys():
+        if account.account_type_group not in list(accounts.keys()):
             accounts[account.account_type_group] = {
                 'accounts': [],
                 'account_type_name': [],
@@ -142,7 +142,7 @@ def account_list_with_balance(session, user_id, end_date=None):
     ## execute query
     accounts = {}
     for account in query.all():
-        if account.account_type_group not in accounts.keys():
+        if account.account_type_group not in list(accounts.keys()):
             accounts[account.account_type_group] = {
                 'accounts': [],
                 'account_type_name': [],
@@ -259,7 +259,8 @@ def operation_list_with_balance(session, account_ids, tags=None,
     query = session.query(Operation)
     if tags is not None:
         for tag in tags:
-            query = query.join(OperationTag).filter(OperationTag.tag_id == tag)
+            operation_tag_alias = aliased(OperationTag)
+            query = query.join(operation_tag_alias).filter(operation_tag_alias.tag_id == tag)
     if len(account_ids) > 0:
         query = query.filter(Operation.account_id.in_((account_ids)))
     if last_n_operations:
@@ -408,7 +409,7 @@ def schedule_transfer(session, schedule_id, max_date):
         end_date = max_date
     ##
     current_date = schedule.start_date
-    print(current_date, end_date)
+    print((current_date, end_date))
     ##
     while str(current_date) <= end_date:
         if schedule.account_2_id:
